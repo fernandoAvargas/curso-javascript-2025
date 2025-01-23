a5ac6adbd79e49fa9d707d8beda54ba2bfe00a31
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-01-23 00:17:55 -0300

    Tipos Primitivos Number e Boolean

---
+++ 003 - Tipos Primitivos/002 - Number/index.js
@@ -0,0 +1,31 @@
+console.log(123);
+console.log("123");
+
+console.log(typeof(123));
+console.log(typeof("123"));
+
+console.log("123" + 1);
+
+console.log("123" - 1);
+
+console.log("123" * 1);
+
+console.log("123" / 1);
+
+console.log("123" % 1);
+
+console.log(Number("123") + 1);
+
+console.log(String("123") - 1);
+
+console.log("123" - String(1));
+
+console.log("123" - "1");
+
+console.log("123" * "2");
+
+console.log("123" + "2");
+
+console.log(Math.sqrt(9));
+
+console.log(Math.pow(2,2));
---
+++ 003 - Tipos Primitivos/003 - Boolean/index.js
@@ -0,0 +1,12 @@
+
+console.log(typeof(true));
+
+console.log(`************************`);
+
+console.log(true);
+console.log(false);
+
+console.log(`************************`);
+
+console.log(1===2);
+console.log(2===2);
