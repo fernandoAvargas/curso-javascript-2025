251ebb0fd1c4de684b4902ba177b6c16a1dc810a
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-01-24 01:25:15 -0300

    Tipos Primitivos - Array

---
+++ 003 - Tipos Primitivos/005 - Array/index.js
@@ -0,0 +1,8 @@
+console.log(["Casa","Apartamento","Sobrado","Chacara"]);
+
+console.log(["Casa","Apartamento","Sobrado","Chacara"].length);
+
+
+["Casa","Apartamento","Sobrado","Chacara"].forEach((res,index) =>{
+    console.log(res,index);
+});
