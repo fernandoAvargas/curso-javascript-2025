2aa9ebfc54e57c4c97d7ad78aafa3d78325f20c7
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-01-24 01:11:14 -0300

    Tipos Primitivos Null Undefined

---
+++ 003 - Tipos Primitivos/004 - Undefined e Null/index.js
@@ -0,0 +1,18 @@
+console.log(undefined);
+
+console.log(null);
+
+let teste;
+
+console.log(teste); // a varável não foi definida então undefined
+
+console.log(undefined);
+
+console.log(undefined === null);
+
+/*
+undefined = Algo que falta definição
+null = Algo que veio nulo
+nullé diferente de undefined
+
+*/
