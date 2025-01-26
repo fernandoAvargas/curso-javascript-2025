1149a8b644a7065812e0b33805f2de2c68e6b6ee
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-01-26 11:30:42 -0300

    Tipos Primitivos - Object

---
+++ 003 - Tipos Primitivos/006 - Object/index.js
@@ -0,0 +1,69 @@
+/*
+    Objeto: Sapato(propriedade) "Tamanho", "Cor"
+    Objeto: Celular(propriedade) "Marca", "Modelo"
+    Objeto: Drone(propriedade) "Alcance", "Peso"
+
+
+   Propriedades / Atributos / Funcionalidades
+*/
+
+console.log(
+    {
+        Sapato : ["Tamanho", "Cor"],
+        Celular : ["Tamanho", "Cor"],
+        Drone : ["Tamanho", "Cor"]
+    }
+);
+
+console.log(
+    {
+        Sapato : {
+            tamanhoSapato : "43"
+        }       
+    }
+);
+
+
+console.log(
+    [{
+        Sapato : {
+            tamanho: "39",
+            modelo: "ABCDEFG",
+            classificaco: "masculina",
+            cor: "marrom"  
+        }       
+    },
+    {
+        Chinelo : {
+            tamanho: "43",
+            modelo: "ABCDEFG",
+            classificaco: "masculina",
+            cor: "branca"    
+        }       
+    },
+    {
+        Sandalia : {
+            tamanho: "41",
+            modelo: "ABCDEFG",
+            classificaco: "feminina",
+            cor: "azul"               
+        }       
+    },
+    {
+        Bota : {
+            tamanho: "43",
+            modelo: "ABCDEFG",
+            classificaco: "feminina",
+            cor: "prata"               
+        }       
+    },
+    {
+        Sapatilha : {
+            tamanho: "43",
+            modelo: "ABCDEFG",
+            classificaco: "feminina",
+            cor: "preta"                       
+        }       
+    }
+]
+);
