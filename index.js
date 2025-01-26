42145d6ec20087bfee1e35403a1b1749534a6cc8
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-01-26 11:41:28 -0300

    Operadores - Adcao

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log(1 + 1);  // cálculo
+
+console.log(2 + 50); // cálculo
+
+console.log("Fernando " + "Assis"); // concatenação
+
+console.log("Nome:" + "Fernando de Assis" + "," + "idade: " + 42); // concatenação
+
+console.log("Nome:" + "Fernando de Assis" + "," + "idade: " + 20 + 20); // concatenação
+
+console.log("Nome:" + "Fernando de Assis" + "," + "idade: " + (20 + 20)); // expressão matemmática
