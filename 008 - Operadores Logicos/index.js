27a1d56ad0e6c2cf204aea64c36cbc360faea518
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-01-26 17:38:30 -0300

    Operadores Logicos

---
+++ 008 - Operadores Logicos/index.js
@@ -0,0 +1,19 @@
+
+/*
+
+&& (E), || (OU), !(Negacao)
+
+*/
+
+console.log(1 === 1 && 2 >= 1);
+
+
+console.log(1 === 1 && 1 >= 2);
+
+console.log(1 === 2 || 1 === 1 );
+
+console.log( 1 != 1 );
+
+console.log(!true);
+
+console.log(!false);
