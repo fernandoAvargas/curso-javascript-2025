0576806206c4d6ceed1bfed10fde1a400d512dcd
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-01-26 17:53:06 -0300

    Precedencia

---
+++ 009 - Precedencia/index.js
@@ -0,0 +1,19 @@
+// Infoma a ordem de onde o calculo da expessão vai começar.
+
+let a = (15 * 20) / 10;
+
+console.log(a);
+
+let b = 15 * (20 / 10);
+
+console.log(b);
+
+const c = (15 * 20) / 10 === 15 && 10 * 10 === 14;
+
+console.log(c);
+
+console.log(false && false);
+
+
+
+
