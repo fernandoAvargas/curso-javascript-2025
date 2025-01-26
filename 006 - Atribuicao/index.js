88ee62631d4b777cd804e2c921ab510dd56750a6
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-01-26 16:57:02 -0300

    Atribuicao

---
+++ 006 - Atribuicao/index.js
@@ -0,0 +1,50 @@
+
+/*
+ Variáveis são nomes simbólicos que armazenam valores na memória do computador.
+ Elas são essenciais para a programação em JavaScript, pois permitem manipular e armazenar dados
+*/
+
+let a = 1;
+
+let b = 2;
+
+console.log(a);
+
+console.log(a+b);
+
+a += 1; 
+
+console.log(a);
+
+// a += 1 é o mesmo que a = a + 1. Veja exemplo abaixo:
+
+a = a + 1;
+
+console.log(a);
+
+a -= 1;
+
+console.log(a);
+
+// a -= 1 é o mesmo que a = a - 1. Veja exemplo abaixo:
+
+a = a -1;
+
+console.log(a);
+
+
+a *= 2;
+
+console.log(a);
+
+a = a * 2;
+
+console.log(a);
+
+a /= 2
+
+console.log(a);
+
+a = a /2
+
+console.log(a);
