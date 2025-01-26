e989b413c225d3403d707fde787c3bfe318bfb4c
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-01-26 12:21:08 -0300

    Reminder

---
+++ 005 - Reminder/index.js
@@ -0,0 +1,15 @@
+// Devolve o resto da divisão. Com isso dá para saber se o divisor é par ou impar. Se der 0 é par
+
+
+console.log(4 % 2); // dois é par
+
+console.log(4 % 3); // três é impar
+
+console.log(4 % "2"); // Nok
+
+console.log(4 % "3"); // Nok
+
+console.log(4 % Number("2")); // dois é par
+
+
+
