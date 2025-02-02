ec3e350ef85dba77bf83819e29ca361b0753269f
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-02-02 00:34:26 -0300

    Condicionais - Falsy - Truthy

---
+++ 011 - Condicionais/002 - Falsy e Truthy/index.js
@@ -0,0 +1,24 @@
+// Falsy é igual a false, 0, "", NaN, undefined e null
+// Truthy é igual a todos os diferentes dos mencionados acima.
+
+
+if("Nome" * 15){
+
+    console.log("Bom dia!");
+
+}else{
+
+    console.log("Boa noite!");
+
+}
+
+
+if(!null){
+
+    console.log("Falsy or False");
+}
+
+if(1+1){
+
+    console.log("True");
+}
