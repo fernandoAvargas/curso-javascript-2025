9f19434f41b6af21149065cdc700f2dd9138b8f3
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-02-02 01:20:37 -0300

    Condiocionais - Ternario

---
+++ 011 - Condicionais/003 - Operador Ternario/index.js
@@ -0,0 +1,18 @@
+
+const nota = 0;
+
+const Resultado = (nota > 5) ? "Aprovdo" : "Reprovado";
+
+console.log(Resultado);
+
+
+// Sem usar ternario
+
+if(Resultado > 5){
+
+    console.log("Aprovdo");
+
+}else{
+
+    console.log("Reprovado"); 
+}
