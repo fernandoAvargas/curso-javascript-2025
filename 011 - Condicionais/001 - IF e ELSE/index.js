2844db24666905905482a05bdeede6f91a8ce1dd
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-02-02 00:17:37 -0300

    Condicionais - IF - ELSE

---
+++ 011 - Condicionais/001 - IF e ELSE/index.js
@@ -0,0 +1,46 @@
+
+let soma = 0 ;
+
+if(soma >= 0){
+
+    console.log("É maior ou igual a zero");
+
+}else{
+
+    console.log("É maior menor que zero");
+
+}
+
+
+if(1 === 0){
+
+    console.log("Um(1) nunca será igual a zero(0). Portanto evite essas condições impossíveis no seu código com valores fixos." +
+                 "Há não ser se for variáveis");
+
+}else if(1===1){
+
+    console.log("Mesama coisa...Um(1) sempre será igual a zero(1). Desse jeito nunca irá para a próxima condição, ou seja, o else");
+
+}else{
+
+    console.log("Nunca entrará aqui. Esse else está só de enfeite");
+}
+
+let a,b = 0;
+
+a = 1;
+b = 2
+
+
+if(a < b){
+
+    console.log("Bom exemplo de utilização de variáveis em condicionais");
+
+}else if(a>=b){
+
+    console.log("Utilizar os condicionais dessa forma está ok");
+
+}else{
+
+    console.log("Nunca entrará aqui. Esse else está só de enfeite");
+}
