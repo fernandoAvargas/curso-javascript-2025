1fe7ace7d3e2171ce58a2cebf849ccc9f050c736
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-02-16 18:02:44 -0300

    Objetos - Como Acessar Valores

---
+++ 014 - Ojetos - Detalhes/003 - Acessar Valores/index.js
@@ -0,0 +1,17 @@
+const Produto = {
+
+ tamanho: 46,
+ marca: "Nome da Marca",
+};
+
+console.log(Produto.marca);
+
+console.log(Produto.tamanho);
+
+console.log(typeof Produto.tamanho);
+
+console.log(Produto.tamanho + 8);
+
+console.log("Essa marca " + Produto.marca + " é muito boa");
+
+console.log(`Essa marca ${Produto.marca} é muito boa`);
