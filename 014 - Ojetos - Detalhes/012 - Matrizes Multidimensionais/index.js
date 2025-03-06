344812e4b88094ebc25e06e341a911a4b2f5ae16
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-03-06 00:04:46 -0300

    Objetos - Matrizes Multidimensionais

---
+++ 014 - Ojetos - Detalhes/012 - Matrizes Multidimensionais/index.js
@@ -0,0 +1,30 @@
+const arr = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
+
+console.log(arr);
+
+console.table(arr);
+
+console.log(arr[0])
+
+console.log(arr[0][0])
+
+
+const arr2 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [[1,2,3],
+    [4,5,6],
+    [7,8,9],
+  ],
+]
+
+console.table(arr2)
+
+console.log(arr2[3]);
+
+console.log(arr2[3][2][2]);
