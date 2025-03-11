fa0e6f2720af7955cd23d6a0bc8948bc8c4823e1
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-03-11 02:41:08 -0300

    Iteracoes - For In

--- 015 - Iteracoes/001 - For In/index.js
+++ 015 - Iteracoes/001 - For In/index.js
@@ -1,3 +1,5 @@
+//O For In é utilizado para casos específicos. É mais indicado para trabalhar com um array de objetos
+
 const arrayListObjects = [
     {
        nome: "nome1",
@@ -13,5 +15,43 @@ const arrayListObjects = [
     } 
    ]
 
+//Vai listar o índice dos objetos 
 
-   
+for(let item in arrayListObjects){
+
+    console.log(item);
+}
+
+console.log("-------------------------------");
+
+for(let item in arrayListObjects){
+
+    console.log(arrayListObjects[item]);
+}
+
+console.log("-------------------------------");
+
+for(let item in arrayListObjects){
+
+    console.log(arrayListObjects[item].nome);
+}
+
+console.log("-------------------------------");
+
+const arrayList = [1,2,3,4,5,6,7,8,9];
+
+//Muito cuidado ao utilizar o For In. Aqui nesse exemplo ele só listou o índice e não o conteúdo de cada índice do array
+
+for(let item in arrayList){
+
+    console.log(item);
+}
+
+console.log("-------------------------------");
+
+// Se quiser acessar o conteúdo do array com For IN terá que fazer isso:
+
+for(let item in arrayList){
+
+    console.log(arrayList[item]);
+}
