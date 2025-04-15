415884de5f90f96a0e72446ae9c929e82f8d20be
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-04-15 00:30:12 -0300

    Formularios - Submeter Formulario + FormData

---
+++ 019 - Formularios/002 - Submetendo Formularios + FormData/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const form = document.forms.namedItem("form");
+
+form.addEventListener("submit", (event) => {event.preventDefault();
+
+    const name = form.name.value;
+    const password = form.password.value;
+
+    const formData = new FormData(form);
+
+    formData.set("name", name);
+
+    formData.set("password", password);
+
+    console.log(formData.has("name"));
+
+    console.log(formData.has("password"));
+
+    console.log(formData.get("name"));
+
+    console.log(formData.get("password"));
+
+    //DELETE
+
+    formData.delete("name");
+
+    console.log(formData.has("name"));
+
+    // if(name && password){
+    //     console.log("Nome:" + name + " " + "Password:" + password);
+
+    //     form.submit();
+
+    // }   
+});
